6eb9b3c79753faff0cea1d80b10ff040a800f2bd
jtliu <13927303506@163.com>
2023-12-22 01:52:12 +0000

    feat: 增加github链接

--- README.md
+++ README.md
@@ -4,9 +4,12 @@
 
 纯手动整理，如果你也是历史爱好者，觉得对你有帮助，可以star一个。
 
+在线地址:https://historyline.online
+
 ## 网站预览
 
 ![PixPin_2023-12-21_17-15-16 (1)](https://github.com/liujuntao123/chines-history-video/assets/22583601/0f199148-ad2c-451b-b044-780d4853f9b2)
+
 
 
 ![PixPin_2023-12-21_17-29-34](https://github.com/liujuntao123/chines-history-video/assets/22583601/52a2a911-0db4-4d83-888b-985ebf66db99)
--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,14 @@
       <HistoryTable />
     </div>
     <detail-drawer :open="open" :links="currentItem.links" :title="currentItem.dynasty" :description="currentItem.description" @onClose="onClose"></detail-drawer>
-    <a-back-top />
+    <a-float-button-group shape="circle" >
+      <a-float-button href="https://github.com/liujuntao123/chines-history-video" target="_blank">
+        <template #icon>
+          <GithubOutlined />
+        </template>
+      </a-float-button>
+      <a-back-top />
+    </a-float-button-group>
   </div>
 </template>
 
@@ -15,6 +22,7 @@ import HeadBanner from './components/HeadBanner.vue';
 import Timeline from './components/AllTimeline.vue';
 import DetailDrawer from './components/DetailDrawer.vue';
 import HistoryTable from './components/HistoryTable.vue';
+import { GithubOutlined } from '@ant-design/icons-vue';
 import { ref } from 'vue';
 const open = ref(false);
 const currentItem = ref({});
